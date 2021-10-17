d1e002f5a734b0091c1a8e1b870a6b99eea5cffc
Hammad <hammad@rpi.ae>
2021-10-17 13:13:23 +0400

    seo test2

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -8,8 +8,10 @@
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
  <title>Online Digital Printing Shop in UAE | Printo - @yield('title')</title>
-        <meta name="description" content="@yield('description')">
-        <meta name="keywords" content="@yield('keywords')">
+        <meta name="description" content="@yield('description')Printo offers Online Digital printing &amp; affordable services to businesses in Dubai, Sharjah, Abu Dhabi &amp; all across the Gulf. Call: 06 534 1113">
+        
+        <meta name="keywords" content="@yield('keywords')online digital printing shop, print shop, printing services, printing near me, photo printing, printing press, print online dubai, print shop near me, Printo Online,  digital printing dubai, digital printing sharjah, uae digital printing, printing press in uae, printing press in Sharjah, printing press in dubai, offset printing in uae, offset printing Sharjah, offset printing dubai, printing companies in uae, printing companies in Sharjah, printing companies in dubai, large format printing, large format printing dubai, printing services,
+     printing press, digital printing, digital printing uae, online printing, offset printing, online printing services dubai, online business card printing dubai">
     
 
     <!-- Styles -->
--- resources/views/manage/layouts/app.blade.php
+++ resources/views/manage/layouts/app.blade.php
@@ -8,9 +8,8 @@
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
     <title>App Name - @yield('title')</title>
-        <meta name="description" content="@yield('Printo offers Online Digital printing &amp; affordable services to businesses in Dubai, Sharjah, Abu Dhabi &amp; all across the Gulf. Call: 06 534 1113')">
-        <meta name="keywords" content="@yield('online digital printing shop, print shop, printing services, printing near me, photo printing, printing press, print online dubai, print shop near me, Printo Online,  digital printing dubai, digital printing sharjah, uae digital printing, printing press in uae, printing press in Sharjah, printing press in dubai, offset printing in uae, offset printing Sharjah, offset printing dubai, printing companies in uae, printing companies in Sharjah, printing companies in dubai, large format printing, large format printing dubai, printing services,
-     printing press, digital printing, digital printing uae, online printing, offset printing, online printing services dubai, online business card printing dubai')">
+        <meta name="description" content="@yield('description')">
+        <meta name="keywords" content="@yield('keywords')">
 
     <link href="{{ asset('css/manage/app.css') }}" rel="stylesheet">
     @livewireStyles
