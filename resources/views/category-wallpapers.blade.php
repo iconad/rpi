db7494edd9556b9d5af947c53bb7d51e1293c1c4
Malik Waleed <malikwaleed@Malik-Waleed-MacBook-Pro.local>
2023-09-15 16:38:39 +0500

    heading text and margin-bottom fixed

--- resources/views/category-wallpapers.blade.php
+++ resources/views/category-wallpapers.blade.php
@@ -15,7 +15,7 @@
         <div class="bg-white p-0 md:p-4">
             <a href=""><img src="{{ asset('images/wallpapers/1.jpg') }}" alt="Image 1" class="w-full h-auto"></a>
             <a href="">
-                <h2 class="md:text-lg text-xs mt-3 md:mt-6">3D Stickers
+                <h2 class="md:text-lg text-lg mb-4 md:mb-0 mt-3 md:mt-6">3D Stickers
                 </h2>
             </a>
         </div>
@@ -23,7 +23,7 @@
         <div class="bg-white p-0 md:p-4">
             <a href=""><img src="{{ asset('images/wallpapers/2.jpg') }}" alt="Image 1" class="w-full h-auto"></a>
             <a href="">
-                <h2 class="md:text-lg text-xs mt-3 md:mt-6">Bedroom
+                <h2 class="md:text-lg text-lg mb-4 md:mb-0 mt-3 md:mt-6">Bedroom
 
 
 
@@ -34,7 +34,7 @@
         <div class="bg-white p-0 md:p-4">
             <a href=""><img src="{{ asset('images/wallpapers/3.jpg') }}" alt="Image 1" class="w-full h-auto"></a>
             <a href="">
-                <h2 class="md:text-lg text-xs mt-3 md:mt-6">Living Room
+                <h2 class="md:text-lg text-lg mb-4 md:mb-0 mt-3 md:mt-6">Living Room
 
 
                 </h2>
@@ -44,7 +44,7 @@
         <div class="bg-white p-0 md:p-4">
             <a href=""><img src="{{ asset('images/wallpapers/4.jpg') }}" alt="Image 1" class="w-full h-auto"></a>
             <a href="">
-                <h2 class="md:text-lg text-xs mt-3 md:mt-6">Dining Room
+                <h2 class="md:text-lg text-lg mb-4 md:mb-0 mt-3 md:mt-6">Dining Room
 
                 </h2>
             </a>
@@ -53,7 +53,7 @@
         <div class="bg-white p-0 md:p-4">
             <a href=""><img src="{{ asset('images/wallpapers/5.jpg') }}" alt="Image 1" class="w-full h-auto"></a>
             <a href="">
-                <h2 class="md:text-lg text-xs mt-3 md:mt-6">Study Room
+                <h2 class="md:text-lg text-lg mb-4 md:mb-0 mt-3 md:mt-6">Study Room
 
                 </h2>
             </a>
@@ -62,7 +62,7 @@
         <div class="bg-white p-0 md:p-4">
             <a href=""><img src="{{ asset('images/wallpapers/6.jpg') }}" alt="Image 1" class="w-full h-auto"></a>
             <a href="">
-                <h2 class="md:text-lg text-xs mt-3 md:mt-6">Corporate
+                <h2 class="md:text-lg text-lg mb-4 md:mb-0 mt-3 md:mt-6">Corporate
 
                 </h2>
             </a>
@@ -71,7 +71,7 @@
         <div class="bg-white p-0 md:p-4">
             <a href=""><img src="{{ asset('images/wallpapers/7.jpg') }}" alt="Image 1" class="w-full h-auto"></a>
             <a href="">
-                <h2 class="md:text-lg text-xs mt-3 md:mt-6">Hospitality
+                <h2 class="md:text-lg text-lg mb-4 md:mb-0 mt-3 md:mt-6">Hospitality
                 </h2>
             </a>
         </div>
@@ -79,14 +79,14 @@
         <div class="bg-white p-0 md:p-4">
             <a href=""><img src="{{ asset('images/wallpapers/8.jpg') }}" alt="Image 1" class="w-full h-auto"></a>
             <a href="">
-                <h2 class="md:text-lg text-xs mt-3 md:mt-6">Education</h2>
+                <h2 class="md:text-lg text-lg mb-4 md:mb-0 mt-3 md:mt-6">Education</h2>
             </a>
         </div>
 
         <div class="bg-white p-0 md:p-4">
             <a href=""><img src="{{ asset('images/wallpapers/9.jpg') }}" alt="Image 1" class="w-full h-auto"></a>
             <a href="">
-                <h2 class="md:text-lg text-xs mt-3 md:mt-6">Medical
+                <h2 class="md:text-lg text-lg mb-4 md:mb-0 mt-3 md:mt-6">Medical
                 </h2>
             </a>
         </div>
@@ -95,7 +95,7 @@
         <div class="bg-white p-0 md:p-4">
             <a href=""><img src="{{ asset('images/wallpapers/10.jpg') }}" alt="Image 2" class="w-full h-auto"></a>
             <a href="">
-                <h2 class="md:text-lg text-xs mt-3 md:mt-6">Canvas Frames
+                <h2 class="md:text-lg text-lg mb-4 md:mb-0 mt-3 md:mt-6">Canvas Frames
                 </h2>
             </a>
         </div>
@@ -103,7 +103,7 @@
         <div class="bg-white p-0 md:p-4">
             <a href=""><img src="{{ asset('images/wallpapers/11.jpg') }}" alt="Image 2" class="w-full h-auto"></a>
             <a href="">
-                <h2 class="md:text-lg text-xs mt-3 md:mt-6"> Cut-Outs
+                <h2 class="md:text-lg text-lg mb-4 md:mb-0 mt-3 md:mt-6"> Cut-Outs
 
 
                 </h2>
